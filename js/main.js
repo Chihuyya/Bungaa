cfa7dad093822adc75d1c6072ff9ec46f87ace50
Chihuyya <sanzhuu911@gmail.com>
2024-11-23 15:07:25 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Lopp yuuu dindungsss😋').split('')
+      const titles = ('Lopp yuuu Mungill😋').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
